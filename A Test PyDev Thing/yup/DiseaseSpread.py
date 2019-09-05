b48a668ff23cad66cd75178fb2d0949faa6faaa3
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-05 17:06:41 +0200

    finished today

--- A Test PyDev Thing/yup/DiseaseSpread.py
+++ A Test PyDev Thing/yup/DiseaseSpread.py
@@ -16,8 +16,9 @@ if __name__ == '__main__':
     
     infectionProbPerContact = 0.05 ##probability to infect person per human-human contact
     recovRate = 0.01 ## fraction of people per day
+    humanContactsPerHour = numpy.zeros(numPlaces, 24) + 0.5 ##average contact one person has each hour, for each place and each hour ##CURRENTLY each hour is indentical
     
-    #initally infected
+    #initially infected
     infectedFrac = numpy.zeros(2)
     infectedFrac[0] = 0.02
     infectedFrac[1] = 0.0
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     S = numpy.zeros(numPlaces) ##susceptible
     I = numpy.zeros(numPlaces) ## infected
     R = numpy.zeros(numPlaces) ##recovered
+
     
     for i in range(0,numPlaces):
         I[i] = placePop[i]*infectedFrac[i]
@@ -35,21 +37,26 @@ if __name__ == '__main__':
         
     ###Iterate
     ##settings
-    timeStep = 1.0 ##units of days
+    timeStep = 1.0 ##units of hours
     
     
     
     
-    
+    totalTime = 60*24 ##in hours
     ##settings end
-    for i in range(0, numPlaces):
-        I[i] = rungeKutta(I[i], S[i], R[i])
     
-    ##Infections Happen
+    ###Iterates for set amount of totalTime
+    for hour in range(0, totalTime):##iteration of 1 hour, calls for changes in each population due to biological factors, then moves people around
+        for i in range(0, numPlaces):##per population, this happens
+            ##get biological population changes
+            changes = numpy.zeros(2) = rungeKuttaInfect(I[i], S[i], R[i], humanContactsPerHour[i][0], recovRate) ##index 0 = recovered population, 1 = newly infected
+            I[i] = I[i] + changes[1] - changes [0]
+            R[i] = R[i] + changes[0]
+            S[i] = S[i] - changes[1]
     
     
     
-    ##Movements Happen
+
     
     print('done')
     #ended
