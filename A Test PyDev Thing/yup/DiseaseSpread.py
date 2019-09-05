d91fc3ffa544514e580564e068f35b2054a34829
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-05 16:43:19 +0200

    yes 2

--- A Test PyDev Thing/yup/DiseaseSpread.py
+++ A Test PyDev Thing/yup/DiseaseSpread.py
@@ -14,6 +14,9 @@ if __name__ == '__main__':
     placePop[0] = 5000
     placePop[1] = 10000
     
+    infectionProbPerContact = 0.05 ##probability to infect person per human-human contact
+    recovRate = 0.01 ## fraction of people per day
+    
     #initally infected
     infectedFrac = numpy.zeros(2)
     infectedFrac[0] = 0.02
@@ -28,6 +31,26 @@ if __name__ == '__main__':
         I[i] = placePop[i]*infectedFrac[i]
         R[i] = 0
         S[i] = placePop[i] - I[i]
+        
+        
+    ###Iterate
+    ##settings
+    timeStep = 1.0 ##units of days
+    
+    
+    
+    
+    
+    ##settings end
+    for i in range(0, numPlaces):
+        I[i] = rungeKutta(I[i], S[i], R[i])
+    
+    ##Infections Happen
+    
+    
+    
+    ##Movements Happen
+    
     print('done')
     #ended
 pass
--- A Test PyDev Thing/yup/ODEsolver.py
+++ A Test PyDev Thing/yup/ODEsolver.py
@@ -6,4 +6,9 @@ Created on Sep. 4, 2019
 
 if __name__ == '__main__':
     print(1+1+1)
+    int valueGiver(int n, int m) 
+{ 
+    answer = m + n
+    return answer 
+}
     pass
