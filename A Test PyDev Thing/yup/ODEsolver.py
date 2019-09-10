362a2bf91e945497da1eeaf9191124feda3d63b6
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-10 16:38:08 +0200

    I think

--- A Test PyDev Thing/yup/ODEsolver.py
+++ A Test PyDev Thing/yup/ODEsolver.py
@@ -1,30 +1,34 @@
 import numpy
 
-def valueGiver(S, I, R, timeStepsPerHour):
+def valueGiver(Sfull, Ifull, Rfull, timeStepsPerHour):
     
-    numPlaces = S.shape[0]
-    runLength = S.shape[1]
+    numPlaces = Sfull.shape[0]
+    runLength = Sfull.shape[1]
+      
+    Stemp = numpy.zeros(numPlaces)
+    Itemp = numpy.zeros(numPlaces)
+    Rtemp = numpy.zeros(numPlaces)
     
-    
-    
-    for pl in range(0, numPlaces):
-        
-        Stemp = S[pl][0]
-        Itemp = I[pl][0]
-        Rtemp = R[pl][0]
-        
-        for h in range(0, runLength):
+    for h in range(0, runLength): ##every hour
 
-            for t in range(0, timeStepsPerHour):
-                
-                Stemp = Stemp + 1/6(F1 + 2*F2 + 2*F3 + F4)
-                
-                Itemp = Itemp + 1/6(G1 + 2*G2 + 2*G3 + G4)
-                
-                Rtemp = Rtemp + 1/6(H1 + 2*H2 + 2*H3 + H4)
+        for t in range(0, timeStepsPerHour): ##number of times each hour
             
-            S[pl][h+1] = numpy.int(Stemp)
-            I[pl][h+1] = numpy.int(Itemp)
-            R[pl][h+1] = numpy.int(Rtemp)
+            for pl in range(0, numPlaces): ##every place
+                
+                S = Stemp[pl]
+                I = Itemp[pl]
+                R = Rtemp[pl]
+                
+                
             
-    return (S, I, R)
+                Stemp[pl] = Stemp[pl] + 1/6(F1 + 2*F2 + 2*F3 + F4)
+                Itemp[pl] = Itemp[pl] + 1/6(G1 + 2*G2 + 2*G3 + G4)
+                Rtemp[pl] = Rtemp[pl] + 1/6(H1 + 2*H2 + 2*H3 + H4)
+        
+    for pl in range(0, numPlaces): ##The current temp values are stored in the history of the 'actual' values
+        
+        Sfull[pl][h+1] = numpy.int(Stemp[pl])
+        Ifull[pl][h+1] = numpy.int(Itemp[pl])
+        Rfull[pl][h+1] = numpy.int(Rtemp[pl])
+            
+    return (Sfull, Ifull, Rfull)
