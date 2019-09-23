c10d56805dabd48c0a54430ce7d84eb0b0ea9353
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-23 15:11:05 +0200

    change1

---
+++ A Test PyDev Thing/Project2/DiscrepScore.py
@@ -0,0 +1,28 @@
+
+import numpy
+def get(originalImg, restoredImg, damagedImg): ##obtain the chi squared discrapency between the restored image and the original image
+    xvals = originalImg.shape[0]
+    yvals = originalImg.shape[1]
+    
+    num = 0 ##the numerator in the equation for chi squared
+    sigmasq = 0 ##sigma squared, the denominator in the equation for chi squared (but without the factor of 1/(n-1))
+    n = 0 ##number of points
+    Imean = 0 ##mean of data in the missing region
+    
+    ##find Imean
+    for x in range(0, xvals):
+        for y in range(0, yvals):
+            if( damagedImg[x][y] == 0 ): ##sum over the pixels of the mission region only
+                n = n + 1
+                Imean = Imean + originalImg[x][y]
+                
+    Imean = Imean/n ##to get the actual mean value
+    
+    ##now, find chi squared
+    for x in range(0, xvals):
+        for y in range(0, yvals):
+            if( damagedImg[x][y] == 0 ): ##sum over the pixels of the mission region only
+                num = num + ( restoredImg[x][y] - originalImg[x][y] )**2
+                sigmasq = sigmasq + ( originalImg - Imean )**2
+    
+    return ((n-1) / n) * (num/sigmasq)
--- A Test PyDev Thing/Project2/Proj2.py
+++ A Test PyDev Thing/Project2/Proj2.py
@@ -10,6 +10,7 @@ import math
 import numpy
 
 import matplotlib.pyplot as plt
+import DiscrepScore
 
 ##FUNCTIONS
 
@@ -24,34 +25,6 @@ def ImgRestoreBasic(img): ##the basic method of restoring the image using the La
 
     return img
 
-def getDiscrepancyScore(originalImg, restoredImg, damagedImg): ##obtain the chi squared discrapency between the restored image and the original image
-    xvals = originalImg.shape[0]
-    yvals = originalImg.shape[1]
-    
-    num = 0 ##the numerator in the equation for chi squared
-    sigmasq = 0 ##sigma squared, the denominator in the equation for chi squared (but without the factor of 1/(n-1))
-    n = 0 ##number of points
-    Imean = 0 ##mean of data in the missing region
-    
-    ##find Imean
-    for x in range(0, xvals):
-        for y in range(0, yvals):
-            if( damagedImg[x][y] == 0 ): ##sum over the pixels of the mission region only
-                n = n + 1
-                Imean = Imean + originalImg[x][y]
-                
-    Imean = Imean/n ##to get the actual mean value
-    
-    ##now, find chi squared
-    for x in range(0, xvals):
-        for y in range(0, yvals):
-            if( damagedImg[x][y] == 0 ): ##sum over the pixels of the mission region only
-                num = num + ( restoredImg[x][y] - originalImg[x][y] )**2
-                sigmasq = sigmasq + ( originalImg - Imean )**2
-    
-    return ((n-1) / n) * (num/sigmasq)
-
-
 ##MAIN
 
 if __name__ == '__main__':
@@ -64,7 +37,7 @@ if __name__ == '__main__':
     print('Beginning with the simple method...')
     restoredImg = ImgRestoreBasic(damagedImg)
     
-    print('Discrepancy Score = ' + getDiscrepancyScore(originalImg, restoredImg, damagedImg))
+    print('Discrepancy Score = ' + DiscrepScore.get(originalImg, restoredImg, damagedImg))
     print('End')
 pass
 
