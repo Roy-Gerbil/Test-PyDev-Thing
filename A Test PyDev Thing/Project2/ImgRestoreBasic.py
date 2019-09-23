b7bbd84c73af8fea2b524d1cbdc5a76f8a642175
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-23 17:20:35 +0200

    dgfhnb

--- A Test PyDev Thing/Project2/ImgRestoreBasic.py
+++ A Test PyDev Thing/Project2/ImgRestoreBasic.py
@@ -1,18 +1,49 @@
-def ImgRestoreBasic(img, mask): ##the basic method of restoring the image using the Laplace equation
+def ImgRestoreBasic(img, mask, relax): ##the basic method of restoring the image using the Laplace equation, takes the image to restore, the mask that damaged it, and the relaxation factor
     ###The method used is successive over-relaxation
     rest = numpy.zeros((mask.shape[0]+2, mask.shape[1]+2)) ##restoration points, using the mask to determine which points are being restored (interior sites are 0 in the mask). larger than mask to accomodate neumann boundaries
     NBs = mask * 0 ##number of neumann boundary points around each point
-    for x in range(mask.shape[0]):
-        for y in range(mask.shape[1]):
+    for x in range(0, mask.shape[0]):
+        for y in range(0, mask.shape[1]):
                 if(mask[x,y] == 0):
                     rest[x+1,y+1] = 0.5 ##initial guess of half-intensity, +1 because oversized matrix
                     ##now set all possible boundary points, using for points outside of image the Neumann condition
                     xd = 1##boundary 1
                     yd = 0##boundary 1
                     if( (x+xd) > img.shape[0] or (y+yd) > img.shape[1] or (x+ xd) < 0 or (y+yd) < 0 ):
-                        rest[x+xd,y+yd] = 0
                         NBs[x, y] += 1
                     else:
                         rest[x+xd,y+yd] = img[x+xd, y+yd]
+                    xd = -1##boundary 2
+                    yd = 0##boundary 2
+                    if( (x+xd) > img.shape[0] or (y+yd) > img.shape[1] or (x+ xd) < 0 or (y+yd) < 0 ):
+                        NBs[x, y] += 1
+                    else:
+                        rest[x+xd,y+yd] = img[x+xd, y+yd]
+                    xd = 0##boundary 3
+                    yd = 1##boundary 3
+                    if( (x+xd) > img.shape[0] or (y+yd) > img.shape[1] or (x+ xd) < 0 or (y+yd) < 0 ):
+                        NBs[x, y] += 1
+                    else:
+                        rest[x+xd,y+yd] = img[x+xd, y+yd]
+                    xd = 0##boundary 4
+                    yd = -1##boundary 4
+                    if( (x+xd) > img.shape[0] or (y+yd) > img.shape[1] or (x+ xd) < 0 or (y+yd) < 0 ):
+                        NBs[x, y] += 1
+                    else:
+                        rest[x+xd,y+yd] = img[x+xd, y+yd]
+    looping = True ##looping until convergence condition is satisfied
+    EPS = 5.13121*10**-7 ##when the largest relative difference between two points after a sweep is lower than this, convergence is satisfied
+    while looping:
+        restTemp = rest ##to store the values of the previous sweep, each sweep
+        for x in range(0, mask.shape[0]):
+            for y in range(0, mask.shape[1]):
+                ##the updating rule
+                rest[x+1,y+1] += (1-relax)*restTemp[x+1, y+1] + (relax / (4-NBs[x,y])) * (rest[x+2, y+1] + rest[x+0, y+1] + rest[x+1, y+0] + rest[x+1, y+2] )
+        if((numpy.abs(rest - restTemp)).max() < EPS):
+            looping = False
     
+    restoredBit = mask##setting the in-boundary part of rest to be used
+    for x in range(0, mask.shape[0]):
+        for y in range(0, mask.shape[1]):
+            restoredBit[x,y] = rest[x+1,y+1]
     return img * mask + rest
--- A Test PyDev Thing/Project2/Proj2.py
+++ A Test PyDev Thing/Project2/Proj2.py
@@ -6,12 +6,10 @@ Created on Sep. 20, 2019
 
 ##IMPORTS
 
-import math
 import numpy
 import matplotlib.pyplot as plt
 import skimage.io
 import skimage.viewer
-import sys
 
 ##FUNCTIONS
 import DiscrepScore
@@ -26,18 +24,18 @@ if __name__ == '__main__':
     ###import image as greyscale, matrix of intensity values
     #Intensity matrix, values from 0 (black) to 1 (white)
     originalImg = skimage.io.imread('image.png',as_grey=True).astype(numpy.float32)
-    plt.imshow(originalImg, cmap='gray', interpolation='nearest');
-    plt.savefig('./Results/file.png')
-    
-    maskforImage = CreateMask.createMask(originalImg, 1)
-    
-    damagedImg = MaskImage.maskImage(originalImg) ##image to be 'restored'
+    maskforImage = CreateMask.createMask(originalImg, 1) ##creates the mask for the image
+    damagedImg = originalImg * maskforImage ##image to be 'restored'
     
     
     
     ##start with simple method
     print('Beginning with the simple method...')
-    restoredImg = ImgRestoreBasic(damagedImg, maskforImage)
+    relaxationFactor = 1.9 ##should be between 0 and 2
+    restoredImg = ImgRestoreBasic(damagedImg, maskforImage, relaxationFactor)
+    
+    plt.imshow(originalImg, cmap='gray', interpolation='nearest');
+    plt.savefig('./Results/file.png')
     
     print('Discrepancy Score = ' + DiscrepScore.discrepScore(originalImg, restoredImg, damagedImg, maskforImage))
     print('End')
