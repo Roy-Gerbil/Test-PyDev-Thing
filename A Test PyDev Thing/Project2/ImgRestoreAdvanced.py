5a7ec47cccf7522cc3812f50cd0dacd14a8608a9
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-28 13:32:02 +0200

    new stuff

--- A Test PyDev Thing/Project2/ImgRestoreAdvanced.py
+++ A Test PyDev Thing/Project2/ImgRestoreAdvanced.py
@@ -1,13 +1,34 @@
 import numpy
-def restore(img, mask): ##a navier-stokes method of restoring the image, takes the image to restore and the mask that was applied
-    ###The method used is successive over-relaxation
-    imgtemp = img ##for iteration
+def restore(I, mask): ##a navier-stokes method of restoring the image, takes the image to restore and the mask that was applied
+    ###The method used involved successive iteration over the masked part of the image (mask[x,y] == 0)
     
-    looping = True
-    while looping:
-        imgtemp = img
+    ##first calculate 2D smoothness estimation L
+    
+    It[x, y] = (delL[x+1, y] . Ndir) |gradI[x,y]| ## derivative in time
+    
+    delL[x, y] = ( L[x+1, y] - L[x-1, y], L[x, y+1] - L[x, y-1]) ## a vector, x and y derivs of L (laplacian)
+    
+    L[x, y] = Ixx[x, y] + Iyy[x, y] ##2D smoothness estimation
+    
+    Ndir = ( -Iy[x, y], Ix[x, y]) / (numpy.sqrt( (Ix[x, y])**2 + (Iy[x, y])**2 )) ##N[x,y,n]/|N[x,y,n]|, also a vector, is the isophote direction
+    
+    B = #projection of delL onto Ndir
+    
+    delt = 0.1 #timestep?
+    Ttimes = 100 ##number of iterations
+    for T in range(0, Ttimes):
         for x in range(0, img.shape[0]):
             for y in range(0, img.shape[1]):
                 if(mask[x,y] == 0):
-                    img[x,y] = imgtemp[x,y] + dt*( -numpy.abs(u) * (( imgtemp[x + numpy.sign(u),y] - imgtemp[x,y] )/ dx) -numpy.abs(v) * (( imgtemp[x,y + numpy.sign(v)] - imgtemp[x,y] )/ dy) + nu(diffdiscret))
-    return (img)
+                    img[x, y] = imgTemp[x, y] + delt * imgTemp[x, y]
+    
+    
+    
+    
+    
+    return img
+
+def g(s):##perona-malik anisotropic diffusion function
+    K = 10**-12 ##diffusion parameter
+    g = 1/(1+(s/K)**2)
+    return g
