c0e5a86b48aa62551a562218a4bf740bfdf6ed82
Alexandros <alexandrospallaris@yahoo.ca>
2019-09-28 16:39:48 +0200

    change

--- A Test PyDev Thing/Project2/ImgRestoreAdvanced.py
+++ A Test PyDev Thing/Project2/ImgRestoreAdvanced.py
@@ -1,5 +1,24 @@
 import numpy
-def restore(I, mask): ##a navier-stokes method of restoring the image, takes the image to restore and the mask that was applied
+def g(s):##perona-malik anisotropic diffusion function
+    K = 10**-12 ##diffusion parameter
+    g = 1/(1+(s/K)**2)
+    return g
+
+def boundary(I, mask):
+    Boundary = numpy.zeros((I.shape[0], I.shape[1], 4))
+    for x in range(0, I.shape[0]):
+        for y in range(0, I.shape[1]):
+            if (mask[x,y] -  mask[x+1,y] > 0): # checking if a point is right boundary
+                Boundary[x,y,0] = 'Right'
+            if (mask[x,y] -  mask[x-1,y] > 0): # checking if a point is left boundary
+                Boundary[x,y,1] = 'Left'
+            if (mask[x,y] -  mask[x,y-1] > 0):
+                Boundary[x,y,2] = 'Top'
+            if (mask[x,y] -  mask[x,y+1] > 0):
+                Boundary[x,y,3] = 'Bottom'
+    return(Boundary)
+
+def restore(I, mask): ##navier-stokes method of restoring the image, takes the image to restore and the mask that was applied
     ###The method used involved successive iteration over the masked part of the image (mask[x,y] == 0)
     
     L = numpy.zeros(I.shape)
@@ -35,29 +54,26 @@ def restore(I, mask): ##a navier-stokes method of restoring the image, takes the
                     if(boundary(I,mask)[x,y,2] == 'Top'): # and boundary(i,mask)[x,y,3] != Bottom  ):
                         Iy[x,y] = (-3*I[x,y] + 4*I[x,y+1]-I[x,y+2])/2
                         Iyy[x,y] = (2*I[x,y] - 5*I[x,y+1] + 4*I[x,y+2]-I[x,y+3])
-                    else:
-                        
                     if(boundary(I,mask)[x,y,3] == 'Bottom'): # and boundary(i,mask)[x,y,2] != Top  ):
                         Iy[x,y] = (3*I[x,y] - 4*I[x,y-1]+I[x,y-2])/2
                         Iyy[x,y] = (2*I[x,y] - 5*I[x,y-1] + 4*I[x,y-2]-I[x,y-3])
-                    else:
-                    
                     if(boundary(I,mask)[x,y,1] == 'Left'): # and boundary(i,mask)[x,y,0] != Right):
                         Ix[x,y] = (-3*I[x,y] + 4*I[x+1,y]-I[x+2,y])/2
-                        Ixx[x,y] = (2*I[x,y] - 5*I[x+1,y] + 4*I[x+2,y]-I[x+3,y]
-                    else:
-                        
+                        Ixx[x,y] = (2*I[x,y] - 5*I[x+1,y] + 4*I[x+2,y]-I[x+3,y]  
                     if(boundary(I,mask)[x,y,0] == 'Right'): #and boundary(i,mask)[x,y,1] != Left ):
                         Ix[x,y] = (3*I[x,y] - 4*I[x-1,y]+I[x-2,y])/2
                         Ixx[x,y] = (2*I[x,y] - 5*I[x-1,y] + 4*I[x-2,y]-I[x-3,y]
-                    else:
-                        
-                    absGrad = getAbsGrad(I)
+                                     
                     L[x, y] = Ixx[x, y] + Iyy[x, y] ##2D smoothness estimation
                     Ndir[x, y] = ( -Iy[x, y], Ix[x, y]) / (numpy.sqrt( (Ix[x, y])**2 + (Iy[x, y])**2 )) ##N[x,y,n]/|N[x,y,n]|, also a vector, is the isophote direction
-                    B = numpy.dot( delL[x, y], Ndir[x, y])#projection of delL onto Ndir
+                    B[x, y] = numpy.dot( delL[x, y], Ndir[x, y])#projection of delL onto Ndir
                     delL[x, y] = ( L[x+1, y] - L[x-1, y], L[x, y+1] - L[x, y-1]) ## a vector, x and y derivs of L (laplacian)
-                    It[x, y] = numpy.dot(delL[x, y], Ndir[x, y]) * absGrad[x, y] ## derivative in time
+                    if(B > 0):
+                        absGrad[x, y] = numpy.sqrt((min(I[x,y]-I[x-1,y], 0)**2) + (max(I[x+1,y]-I[x,y], 0)**2) + (min(I[x,y]-I[x,y-1], 0)**2) + (max(I[x,y+1]-I[x,y], 0)**2))
+                    elif(B < 0):
+                        absGrad[x, y] = numpy.sqrt((max(I[x,y]-I[x-1,y], 0)**2) + (min(I[x+1,y]-I[x,y], 0)**2) + (max(I[x,y]-I[x,y-1], 0)**2) + (min(I[x,y+1]-I[x,y], 0)**2)) 
+
+                    It[x, y] = B[x, y] * absGrad[x, y] ## derivative in time
                     ##here multiply B by a slop-limited version of the norm of the gradient of the image
                     
                     if(mask[x,y] == 0): ##only actually update pixels inside the mask
@@ -65,11 +81,34 @@ def restore(I, mask): ##a navier-stokes method of restoring the image, takes the
                         
         for B in range(0, 2): ##the diffusion loop
             print('B = ' +numpy.str(B))
-            Ix = numpy.gradient(I)[0]
-            Iy = numpy.gradient(I)[1]
-            absGrad = getAbsGrad(I)
+            #Ix = numpy.gradient(I)[0]
+            #Iy = numpy.gradient(I)[1]
+            
             for x in range(0, I.shape[0]):
                 for y in range(0, I.shape[1]):
+                    
+                    if(boundary(I,mask)[x,y,2] == 'Top'): # and boundary(i,mask)[x,y,3] != Bottom  ):
+                        Iy[x,y] = (-3*I[x,y] + 4*I[x,y+1]-I[x,y+2])/2
+                        Iyy[x,y] = (2*I[x,y] - 5*I[x,y+1] + 4*I[x,y+2]-I[x,y+3])
+                    if(boundary(I,mask)[x,y,3] == 'Bottom'): # and boundary(i,mask)[x,y,2] != Top  ):
+                        Iy[x,y] = (3*I[x,y] - 4*I[x,y-1]+I[x,y-2])/2
+                        Iyy[x,y] = (2*I[x,y] - 5*I[x,y-1] + 4*I[x,y-2]-I[x,y-3])
+                    if(boundary(I,mask)[x,y,1] == 'Left'): # and boundary(i,mask)[x,y,0] != Right):
+                        Ix[x,y] = (-3*I[x,y] + 4*I[x+1,y]-I[x+2,y])/2
+                        Ixx[x,y] = (2*I[x,y] - 5*I[x+1,y] + 4*I[x+2,y]-I[x+3,y]  
+                    if(boundary(I,mask)[x,y,0] == 'Right'): #and boundary(i,mask)[x,y,1] != Left ):
+                        Ix[x,y] = (3*I[x,y] - 4*I[x-1,y]+I[x-2,y])/2
+                        Ixx[x,y] = (2*I[x,y] - 5*I[x-1,y] + 4*I[x-2,y]-I[x-3,y]
+                                    
+                    L[x, y] = Ixx[x, y] + Iyy[x, y] ##2D smoothness estimation
+                    Ndir[x, y] = ( -Iy[x, y], Ix[x, y]) / (numpy.sqrt( (Ix[x, y])**2 + (Iy[x, y])**2 )) ##N[x,y,n]/|N[x,y,n]|, also a vector, is the isophote direction
+                    B[x, y] = numpy.dot( delL[x, y], Ndir[x, y])#projection of delL onto Ndir
+                    if(B > 0):
+                        absGrad[x, y] = numpy.sqrt((min(I[x,y]-I[x-1,y], 0)**2) + (max(I[x+1,y]-I[x,y], 0)**2) + (min(I[x,y]-I[x,y-1], 0)**2) + (max(I[x,y+1]-I[x,y], 0)**2))
+                    elif(B < 0):
+                        absGrad[x, y] = numpy.sqrt((max(I[x,y]-I[x-1,y], 0)**2) + (min(I[x+1,y]-I[x,y], 0)**2) + (max(I[x,y]-I[x,y-1], 0)**2) + (min(I[x,y+1]-I[x,y], 0)**2)) 
+
+                    
                     gArray[x, y] = g(absGrad[x, y])
             diffusion = 1/dt * (numpy.gradient( gArray * Ix )[0] + numpy.gradient( gArray * Iy )[1])
             I = I + dt*diffusion     
@@ -88,33 +127,3 @@ def restore(I, mask): ##a navier-stokes method of restoring the image, takes the
     
     
     return I
-
-
-def getAbsGrad(I):
-    absGrad = numpy.zeros(I.shape))
-    for x in range(0, I.shape[0]):
-        for y in range(0, I.shape[1]):
-            if(B > 0):
-                absGrad[x, y] = numpy.sqrt((min(I[x,y]-I[x-1,y], 0)**2) + (max(I[x+1,y]-I[x,y], 0)**2) + (min(I[x,y]-I[x,y-1], 0)**2) + (max(I[x,y+1]-I[x,y], 0)**2))
-            elif(B < 0):
-                absGrad[x, y] = numpy.sqrt((max(I[x,y]-I[x-1,y], 0)**2) + (min(I[x+1,y]-I[x,y], 0)**2) + (max(I[x,y]-I[x,y-1], 0)**2) + (min(I[x,y+1]-I[x,y], 0)**2)) 
-    return absGrad
-
-def g(s):##perona-malik anisotropic diffusion function
-    K = 10**-12 ##diffusion parameter
-    g = 1/(1+(s/K)**2)
-    return g
-
-def boundary(I, mask):
-    Boundary = numpy.zeros((I.shape[0], I.shape[1], 4))
-    for x in range(0, I.shape[0]):
-        for y in range(0, I.shape[1]):
-            if (mask[x,y] -  mask[x+1,y] > 0): # checking if a point is right boundary
-                Boundary[x,y,0] = 'Right'
-            if (mask[x,y] -  mask[x-1,y] > 0): # checking if a point is left boundary
-                Boundary[x,y,1] = 'Left'
-            if (mask[x,y] -  mask[x,y-1] > 0):
-                Boundary[x,y,2] = 'Top'
-            if (mask[x,y] -  mask[x,y+1] > 0):
-                Boundary[x,y,3] = 'Bottom'
-    return(Boundary)
